dbb3ca42bd2afebc2228b69d4f60ad501e802c34
sayrung <32380670+sayrung@users.noreply.github.com>
2017-12-19 12:53:24 +0700

    Project1
    send projectA1

---
+++ ICT13580032A/ICT13580032APage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13580032A
+{
+    public partial class ICT13580032APage : ContentPage
+    {
+        public ICT13580032APage()
+        {
+            InitializeComponent();
+
+            okButton.Clicked += OkButton_Clicked;
+
+            gendarPicker.Items.Add("ชาย");
+            gendarPicker.Items.Add("หญิง");
+
+            mySlider.ValueChanged += MySlider_ValueChanged;
+
+            myStepper.ValueChanged += MyStepper_ValueChanged;
+        }
+
+        void OkButton_Clicked(object sender, System.EventArgs e)
+        {
+            firstNameLabel.Text = firstNameEntey.Text;
+        }
+
+        void MySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+            valueLabel.Text = value.ToString();
+        }
+
+        void MyStepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+            valueLabel.Text = value.ToString();
+        }
+    }
+}
